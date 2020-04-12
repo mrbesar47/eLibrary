8ca4d59fb209bf0f79254045ffc2519a1f643041
Besar <limanibesar@gmail.com>
2020-04-12 20:43:03 +0200

    NewSearch

---
+++ eLibrary/Controllers/SearchController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eLibrary.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eLibrary.Controllers
+{
+    public class SearchController : Controller
+    {
+        
+        private readonly ApplicationDbContext _context;
+
+        public SearchController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [Authorize]
+        public async Task<IActionResult> Index(string Request)
+        {
+            var Search = from m in _context.Book select m;
+
+
+            if (!String.IsNullOrEmpty(Request))
+            {
+                Search = Search.Where(s => s.Name.ToLower().Contains(Request.ToLower()) || s.Description.ToLower().Contains(Request.ToLower()) || s.Author.ToLower().Contains(Request.ToLower()));            
+            }   
+
+            return View(await Search.ToListAsync());
+        }
+
+    }
+}
---
+++ eLibrary/Data/Migrations/20200412183046_testing.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eLibrary.Data.Migrations
+{
+    public partial class testing : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+SET IDENTITY_INSERT [dbo].[Book] ON
+INSERT INTO [dbo].[Book] ([Id], [Name], [Description], [Author], [DateAdded], [PublishDate]) VALUES (6, N'Test', N'Test', N'B', N'2020-04-02 13:00:00', N'2020-04-02 13:00:00')
+INSERT INTO [dbo].[Book] ([Id], [Name], [Description], [Author], [DateAdded], [PublishDate]) VALUES (9, N'Test', N'T', N'A', N'2020-04-12 13:00:00', N'2020-04-05 12:59:00')
+INSERT INTO [dbo].[Book] ([Id], [Name], [Description], [Author], [DateAdded], [PublishDate]) VALUES (10, N'Let', N'Testing', N'Let me test', N'2020-04-12 12:59:00', N'2020-04-06 12:59:00')
+SET IDENTITY_INSERT [dbo].[Book] OFF");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
